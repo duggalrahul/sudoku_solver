6138060062ef04ff4b770af56e9d8580b1f6f325
Rahul Duggal <rahulduggal2608@gmail.com>
2014-05-08 00:13:09 +0530

    Improvement 1

--- lib/solve.rb
+++ lib/solve.rb
@@ -1,20 +1,3 @@
-# The program loads sudoku puzzle from file then
-# it tries to solve using various solving algorithms.
-# First algorithm is the simplest: brute force
-#
-# Author:: Krunoslav Husak (mailto:krunoslav.husak@gmail.com)
-# Copyright:: Copyright (c) 2011 Krunoslav Husak
-# License:: Distributes under the same terms as Ruby
-
-# This class implements algorithms for solving sudoku puzzle.
-# Constructor requries one argument: path to textual file containing sudoku puzzle.
-# Sudoku can be any size (4x4, 9x9, 25x25, ...)
-# Cells where value is unknows should be 0. Example for file contents:
-# 0 0 1 0
-# 0 2 0 0
-# 3 0 0 0
-# 0 0 0 1
-
 class SudokuSolver
   attr_reader :grid
 
